5800524cc86764cd082379ddfaaa53ba3bbc7433
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-05-31 01:05:02 +0200

    exercise done

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@
 
 const playButton = document.getElementById('play');
 const messageText = document.getElementById('message');
+const timerShow = document.getElementById('timer')
 
 //funzione per creare un numero randomico tra min e max
 function randomNumGenerator(min, max){
@@ -22,10 +23,31 @@ function createRandomArray(min, max, lengthOfArray){
 
 //funzione che confronta i numeri che fa inserire con un array
 function arrayNumUtente(array, arrayControllo){
-    for(let i = 0; i < arrayControllo.length ; i++){
-        let num = parseInt(prompt('Inserisci i numeri che hai visto uno alla volta'))
-        if (arrayControllo.includes(num)){
-            array.push(num)
+    for(let x = 0; x < arrayControllo.length; x++){
+        let num = prompt('Inserisci i numeri che hai visto uno alla volta').trim();
+        let numArray = num.split(' ');
+        const quantityOfArrayItems = numArray.length
+        //se viene annullato il prompt conclude subito l'operazione
+        if(num === null){
+            num = ' '
+            x = arrayControllo.length
+        }
+        //ciclo che rimuove gli spazi vuoti nell'array
+        for(let i = 0; i < quantityOfArrayItems ; i++){
+            const indexArray = quantityOfArrayItems - 1 - i
+            if (numArray[indexArray] === ''){
+                numArray.splice(indexArray, 1);
+            } 
+        }
+        //ciclo che inserisce i valori validi all'interno dei numeri esatti
+        for(let i = 0; i < arrayControllo.length ; i++){
+            if (arrayControllo.includes(parseInt(numArray[i])) && !array.includes(numArray[i])){
+                array.push(numArray[i]);
+            }
+            if (numArray.length !== 1){
+                x++
+            }
+            
         }
     }
 }
@@ -34,24 +56,35 @@ function arrayNumUtente(array, arrayControllo){
 playButton.addEventListener('click', function() {
     let numeriGioco = createRandomArray(1, 99, 5);
     let numeriGiusti = [];
-    messageText.innerText = numeriGioco;
+    let time = 5;
+    setTimeout(function(){
+        messageText.innerText = numeriGioco;
+    }, 1000)
+    //funzione timer
+    let timer = setInterval(function(){
+        timerShow.innerText = time
+        if(time === 0){
+            clearInterval(timer)
+        }
+        time--
+    }, 1000)
     //funzione che toglie i numeri dallo schermo
     setTimeout(function(){
         messageText.innerText = 'Ricorda i numeri';
-        console.log(numeriGioco)//cheat
-    }, 5000)
+        console.log(numeriGioco);//cheat
+    }, 6000)
     //funzione che scrive un messaggio in base al risultato
     setTimeout(function(){
-        arrayNumUtente(numeriGiusti, numeriGioco)
+        arrayNumUtente(numeriGiusti, numeriGioco);
         if(numeriGiusti.length === 0){
-            messageText.innerText = 'Non hai ricordato nemmeno un numero'
+            messageText.innerText = 'Non hai ricordato nemmeno un numero';
         } else if(numeriGiusti.length === 1){
             messageText.innerHTML = `Hai individuato un solo numero: ${numeriGiusti}`;
         } else if(numeriGiusti.length === numeriGioco.length){
-            messageText.innerText = `Complimenti, hai ricordato tutti i numeri: ${numeriGiusti}`
+            messageText.innerText = `Complimenti, hai ricordato tutti i numeri: ${numeriGiusti}`;
         } else{
             messageText.innerHTML = `Hai individuato ${numeriGiusti.length} numeri: ${numeriGiusti}`;
         }
-    }, 5100)
+    }, 6100)
 
 })
